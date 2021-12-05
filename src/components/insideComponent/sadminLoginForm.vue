7246ba7ee790bfaa603840c171802e39aff8619c
FEIZZER <2208146837@qq.com>
2021-12-05 23:06:04 +0800

    super admin

---
+++ src/components/insideComponent/sadminLoginForm.vue
@@ -0,0 +1,112 @@
+<!--
+ * @Author: feizzer
+ * @Date: 2021-12-05 13:50:58
+ * @LastEditors: feizzer
+ * @LastEditTime: 2021-12-05 20:50:18
+ * @Description: 
+-->
+<template>
+  <div>
+    <el-form ref="form" :model="formData" label-width="60px" label-position="left" :rules="rules">
+      <el-form-item>
+        <div class="login-title">超级管理员登录</div>
+      </el-form-item>
+      <el-form-item label="账户" prop="account">
+        <el-input v-model="formData.account"></el-input>
+      </el-form-item>
+      <el-form-item prop="password" label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" size="normal" @click="doLogin">登录</el-button>
+        <el-button type="info" @click="reset()">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import {superAdminRoutes} from '@/router/index'
+export default {
+  name: "PurchasingPlatformVueLoginform",
+  components: {
+      superAdminRoutes
+  },
+
+  data() {
+    return {
+      formData: {
+        account: "superAccount",
+        password: "123456"
+      },
+      rules: {
+        account: [
+          {required: true, message: '请输入用户名', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, min: 6, max:20, message: '请输入6~20位密码' ,trigger: ['blur', 'change']}
+        ]
+      }
+    };
+  },
+
+  mounted() {
+      console.log('this is state', this.$store.state.logined)
+  },
+
+  methods: {
+    reset() {
+      this.formData.account = '',
+      this.formData.password = ''
+    },
+    doLogin() {
+      this.$refs.form.validate( res => {
+        this.$http.get('/getTokenByUserName', {
+          params: {
+            username: this.formData.account,
+            password: this.formData.password,
+            roleType: '772a7b13dabc015c495d9056aed54a85'
+
+          }
+        })
+        .then( res => {
+          if(res) {
+            console.log(res)
+            let data = res.data
+            if (data.code === 200 && data.success) {
+                localStorage.setItem('token', data.data)
+                this.$store.state.logined = true
+                this.$store.state.menus = superAdminRoutes[0].children;
+                // this.$router.addRoutes(superAdminRoutes);
+                this.$router.push({ path: '/home',})
+            }
+            else {
+              this.$message({
+                type: 'warning',
+                message: data.msg,
+              })
+            }
+          } else{
+            this.$message({
+              type: 'warning',
+              message: '请正确填写登录信息'
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      })
+
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.login-title {
+  font-size: 20px;
+  margin: 0 30px;
+  color: #555555;
+}
+</style>
---
+++ src/components/logined/adminSuper/orgnizationPage.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author: feizzer
+ * @Date: 2021-12-05 20:56:53
+ * @LastEditors: feizzer
+ * @LastEditTime: 2021-12-05 20:56:53
+ * @Description: 
+-->
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PurchasingPlatformVueOrgnizationpage',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="css" scoped>
+
+</style>
