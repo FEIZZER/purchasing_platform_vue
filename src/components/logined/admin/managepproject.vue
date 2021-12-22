6047a50674184ae20eef8c165c0ddc71c7c69e64
FEIZZER <2208146837@qq.com>
2021-12-22 13:42:52 +0800

    admin对项目的处理，一个页面数据显示bug为修复

--- src/components/logined/admin/managepproject.vue
+++ src/components/logined/admin/managepproject.vue
@@ -2,7 +2,7 @@
  * @Author: feizzer
  * @Date: 2021-12-18 17:21:19
  * @LastEditors: feizzer
- * @LastEditTime: 2021-12-18 22:16:27
+ * @LastEditTime: 2021-12-22 13:40:58
  * @Description: 
 -->
 <template>
@@ -14,7 +14,7 @@
             <el-button type="primary" size="small" style="margin-left: auto"
                 @click="showAdd">新增</el-button>
             <el-button type="danger" size="small" style="margin-right: 50px"
-                >删除</el-button>
+                @click="doDeletes">删除</el-button>
         </div>
         <div class="table_area">
             <el-table :data="projects" border @selection-change="handleChange">
@@ -35,7 +35,7 @@
                 <el-table-column label="操作" align="center">
                     <template slot-scope="scope">
                         <el-button size="mini" type="success" @click="showChange(scope.row.id)">修改</el-button>
-                        <el-button size="mini" type="danger">删除</el-button>
+                        <el-button size="mini" type="danger" @click="doDelete(scope.row.id)">删除</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -68,16 +68,23 @@
         </el-dialog>
         <el-dialog :visible.sync="change_visible" title="修改项目信息">
             <el-form :model="change_detail">
-                <el-form-item label="编号" label-width="80px" prop="">
-                    <el-input v-model="change_detail"></el-input>
+                <el-form-item label="编号" label-width="80px">
+                    <el-input v-model="change_detail.id" disabled></el-input>
                 </el-form-item>
                 <el-form-item label="项目名" label-width="80px" prop="productName">
                     <el-input v-model="change_detail.projectName"></el-input>
                 </el-form-item>
                 <el-form-item label="类型" label-width="80px" prop="productType">
+                    <el-select v-model="change_detail.productType" @change="getProdcutsByType">
+                        <el-option v-for="type in types" :key="type.id" :value="type.id" 
+                            :label="type.productType"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="产品" label-width="80px" prop="product">
-                    <el-input v-model="change_detail.product"></el-input>
+                    <el-select v-model="change_detail.product" multiple>
+                        <el-option v-for="pro in products" :key="pro.id"
+                            :value="pro.id" :label="pro.productName"></el-option>
+                    </el-select>
                 </el-form-item>
                 <el-form-item label="项目描述" label-width="80px" prop="description">
                     <el-input v-model="change_detail.description"></el-input>
@@ -128,17 +135,102 @@ export default {
         };
     },
 
-    mounted() {
-        
-    },
+    mounted() {},
     created() {
         this.initAccountInfo()
         this.getProjects()
         this.getProductType()
     },
     methods: {
+        doDelete(id){
+            this.$http({
+                method: 'delete',
+                url: '/deleteProjectInfoById',
+                params: {
+                    id: id
+                }
+            })
+            .then(res=>{
+                let data = res.data
+                if (data.success) {
+                    this.$message({
+                        type: 'info',
+                        message: '已删除该项目信息'
+                    })
+                    this.getProjects()
+                }else{
+                    this.$message({
+                        type: 'warning',
+                        message: data.msg
+                    })
+                }
+            })
+            .catch(err=>{
+
+            })
+        },
+        doDeletes(){
+            this.selected.forEach(pro=>{
+                this.doDelete(pro.id)
+            })
+        },
+        showChange(id){
+            this.change_detail = {}
+            this.$http.get('/getProjectInfoById', {
+                params:{
+                    projectId: id
+                }
+            })
+            .then(res => {
+                let data = res.data
+                if (data.success) {
+                    this.change_detail = data.data
+                    this.handleData(JSON.parse(JSON.stringify(this.change_detail)))
+                    console.log(this.change_detail)
+                    this.change_visible = true
+                }else{
+                    this.$message({
+                        type: 'warning',
+                        message: data.msg
+                    })
+                }
+            })
+            .catch(err => {
+                console.error(err)
+            })
+        },
         submitChange() {
-            
+            this.$http({
+                method: 'put',
+                url: '/updateProject',
+                data: {
+                    accountId: this.accountInfo.accountId,
+                    id: this.change_detail.id,
+                    productType: this.change_detail.productType,
+                    product: this.change_detail.product,
+                    projectName: this.change_detail.projectName,
+                    description: this.change_detail.description
+                }
+            })
+            .then(res=>{
+                let data = res.data
+                if (data.success) {
+                    this.$message({
+                        type: 'success',
+                        message: '修改成功'
+                    })
+                    this.getProjects()
+                    this.change_visible = false
+                } else{
+                    this.$message({
+                        type: 'warning',
+                        message: data.msg
+                    })
+                }
+            })
+            .catch(err=>{
+                console.errorP(err)
+            })
         },
         submitAdd() {
             this.$http({
@@ -215,7 +307,8 @@ export default {
             })
         },
         getProdcutsByType(id) {
-            console.log(id)
+            this.add_detail.product = []
+            this.change_detail.product = []
             this.$http.get('/getProductBriefInfosByTypeId', {
                 params:{
                     typeId: id
@@ -248,12 +341,13 @@ export default {
             .then(res=>{
                 let data = res.data
                 if (data.success) {
-                    obj.productType = data.data.productTypeInfos[0].typeId
+                    this.change_detail.productType = data.data.productTypeInfos[0].typeId
+                    this.getProdcutsByType(this.change_detail.productType)
                     let a = []
                     obj.productNames.forEach(pro => {
                         a.push(pro.id)
                     })
-                    obj.productNames = a
+                    this.change_detail.product = JSON.parse(JSON.stringify(a))
                 }else{
                     this.$message({
                         type: 'warning',
